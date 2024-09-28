372e9c7755b1e7617cba2a4ee63b4e0268c562bf
Yevhen <e.v.ilnytskyi@gmail.com>
2024-09-28 18:24:48 +0300

    Fixed fetch funks

--- bike-front/src/components/footer/Footer.jsx
+++ bike-front/src/components/footer/Footer.jsx
@@ -17,12 +17,9 @@ const Footer = () => {
   useEffect(() => {
     const fetchNavLinks = async () => {
       try {
-        const { data } = await fetchData(
-          "/wp-json/rae/v1/header-footer?header_location_id=hcms-menu-header&footer_location_id=hcms-menu-footer"
-        );
-        if (data) {
-          setNavLinks(data.header.headerMenuItems);
-        }
+        fetchData("links").then((linksData) => {
+          linksData && setNavLinks(linksData.data.header.headerMenuItems);
+        });
       } catch (error) {
         console.error("Failed to fetch navigation links:", error);
       }
